d3b5947c44754d6cb46566070aa748587b413a04
Sibahia <daniel139alejandro@gmail.com>
2023-07-16 12:17:14 -0400

    update res.send

--- backend/routes/verify.js
+++ backend/routes/verify.js
@@ -2,10 +2,13 @@ const { Router } = require('express');
 const { verifyAuth } = require('../module/verifyToken.js');
 const router = new Router();
 
+router.get('/auth/:token', function(req, res) {
+    res.sendFile('verify.html', { root: 'frontend/public' });
+});
+
 router.post('/auth/:token', verifyAuth, function (req, res) {
     
     let token = req.params.token;
-    console.log('Token: ' + token)
 
     verifyAuth(token, req, res);
 
